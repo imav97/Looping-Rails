475e64660111aeb483f6d8f5d50de37e8009a7b7
Jose Tadeo Monagas <josetmonagas97@gmail.com>
2020-10-04 22:43:56 -0300

    Selector Fix

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const MOUSE_CORRECTION: Vector2 = Vector2(0, 0)
+const MOUSE_CORRECTION: Vector2 = Vector2(10, 10)
 const MAX_RAIL_COUNT: int = 9
 
 export var rail_inventory: int = MAX_RAIL_COUNT setget _set_rail_count
@@ -21,13 +21,13 @@ func _physics_process(delta):
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and Input.is_mouse_button_pressed(BUTTON_LEFT):
-		var pos: Vector2 = event.position
+		var pos: Vector2 = tilemap.map_to_world(tilemap.world_to_map(get_global_mouse_position()))
 		var cell: int = tilemap.get_cellv(tilemap.world_to_map(pos))
 		if (cell == tilemap.INVALID_CELL):
 			if !popup.visible:
-				popup.set_position(get_global_mouse_position().snapped(tilemap.cell_size))
+				popup.set_position(pos + MOUSE_CORRECTION)
 				popup.show_modal()
-				rail_position = get_global_mouse_position().snapped(tilemap.cell_size)
+				rail_position = pos
 		else:
 			tilemap.set_cellv(tilemap.world_to_map(pos), -1)
 			rail_inventory += 1
@@ -38,8 +38,12 @@ func _unhandled_input(_event: InputEvent) -> void:
 	var mouse_position: Vector2 = get_global_mouse_position()
 	rail_counter.set_position(mouse_position + MOUSE_CORRECTION)
 	
+	var world_to_map = tilemap.world_to_map(mouse_position)
+	var map_to_world = tilemap.map_to_world(world_to_map)
+	
+	
 	if !popup.visible:
-		var position: Vector2 = mouse_position.snapped(tilemap.cell_size)
+		var position: Vector2 = map_to_world
 		cell_selector.position = position
 
 
@@ -47,6 +51,7 @@ func _on_PopupMenu_rail_pressed(rail):
 	if rail_inventory > 0:
 		tilemap.set_cellv(tilemap.world_to_map(rail_position), tilemap.tile_set.find_tile_by_name(rail))
 		rail_inventory -= 1
+		
 
 
 func _set_rail_count(value: int):
